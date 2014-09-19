6f3105de3f5f0cbebff50497a908db884058955a
shewlayce <shewlayce@gmail.com>
2014-09-18 21:31:01 -0400

    page generation complete

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,11 @@ App = (function(){
   $.ajax({
     url: "torrents",
     success: function(json){
-   	console.log(json);
+	var torrents = $.parseJSON(json);
+        for (var key in torrents) {
+         $('#torrentTable').append('<tr><td>'+torrents[key].name+'</td><td><a href="'+torrents[key].url+'"><button type="button" class="btn btn-primary">Download</button></a></td></tr>');
+
+        }
     }	  
   });
  }
