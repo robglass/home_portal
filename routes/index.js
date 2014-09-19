15ad1f6269ba898ec37890d07fff2e091a3f44d0
Will DeBerry <willdeberry@gmail.com>
2014-09-18 22:29:55 -0400

    House keeping

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/torrents', function(req,res){
-  fs.readFile('/var/www/home/torrent_list.json', 'utf8', function(err, data){
+  fs.readFile('/var/www/home_portal/public/torrent/torrent_list.json', 'utf8', function(err, data){
   	res.send(data);
   })
 })
