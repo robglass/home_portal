f53ed1eb740675600636be3fadf24467d9cf4988
shewlayce <shewlayce@gmail.com>
2014-09-18 20:26:55 -0400

    setup Torrent api to serve from file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/torrents', function(req,res){
+  fs.readFile('/var/www/home/torrent_list.json', 'utf8', function(err, data){
+  	res.send(data);
+  })
+})
 module.exports = router;
