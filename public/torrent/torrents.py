2cc0a43a898f3dec4acb1849f731120c98b841a7
Will DeBerry <willdeberry@gmail.com>
2014-09-19 22:39:49 -0400

    torrent: scrap across multiple pages and concat all into one long page.

--- public/torrent/torrents.py
+++ public/torrent/torrents.py
@@ -28,56 +28,54 @@ def auth():
     # Authenticate
     r = session.post(authurl, data=formdata, headers=req_headers, allow_redirects=False)
     if r.status_code == 302:
-        gather(session)
+        parse(session)
     else:
         print('Error: Unable to Authenticate')
         sys.exit(1)
 
-def gather(session):
-    targeturl = 'http://torrentleech.org/torrents/browse/index/categories/13%2C14/page/1'
-
-    # Read data
-    r = session.get(targeturl)
-    data = r.text
-
-    parse(data)
-
-def parse(data):
-    soup = BeautifulSoup(data)
-    torrentEven = []
-    torrentOdd = []
+def parse(session):
     torrentList = []
     tmpFile = open('torrent_list.tmp', 'w')
+    torrentEven = []
+    torrentOdd = []
 
-    for info in soup.find_all(class_="even"):
-        torrent = {}
-        name = info.find(class_="title")
-        url = info.find(class_="quickdownload").find('a').get('href')
-        date = info.find(class_="name")
-        dateRegex = re.match("^.*(\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}[:]\d{2}).*$",str(date),re.DOTALL)
-        size = info.find_all('td')
-        sizeRegex = re.match("^.*[<td>](\d.*\s\w\w)[</td>].*$",str(size),re.DOTALL)
-        torrent['name'] = str(name.string)
-        torrent['url'] = 'http://torrentleech.org' + url
-        torrent['date'] = dateRegex.group(1)
-        torrent['size'] = sizeRegex.group(1)
-        torrentEven.append(torrent)
+    for x in range(1,6):
+        targeturl = 'http://torrentleech.org/torrents/browse/index/categories/13%2C14/page/' + str(x)
+        print(targeturl)
+        r = session.get(targeturl)
+        data = r.text
+        soup = BeautifulSoup(data)
 
-    for info in soup.find_all(class_="odd"):
-        torrent = {}
-        name = info.find(class_="title")
-        url = info.find(class_="quickdownload").find('a').get('href')
-        date = info.find(class_="name")
-        dateRegex = re.match("^.*(\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}[:]\d{2}).*$",str(date),re.DOTALL)
-        size = info.find_all('td')
-        sizeRegex = re.match("^.*[<td>](\d.*\s\w\w)[</td>].*$",str(size),re.DOTALL)
-        torrent['name'] = str(name.string)
-        torrent['url'] = 'http://torrentleech.org' + url
-        torrent['date'] = dateRegex.group(1)
-        torrent['size'] = sizeRegex.group(1)
-        torrentOdd.append(torrent)
+        for info in soup.find_all(class_="even"):
+            torrent = {}
+            name = info.find(class_="title")
+            url = info.find(class_="quickdownload").find('a').get('href')
+            date = info.find(class_="name")
+            dateRegex = re.match("^.*(\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}[:]\d{2}).*$",str(date),re.DOTALL)
+            size = info.find_all('td')
+            sizeRegex = re.match("^.*[<td>](\d.*\s\w\w)[</td>].*$",str(size),re.DOTALL)
+            torrent['name'] = str(name.string)
+            torrent['url'] = 'http://torrentleech.org' + url
+            torrent['date'] = dateRegex.group(1)
+            torrent['size'] = sizeRegex.group(1)
+            torrentEven.append(torrent)
+
+        for info in soup.find_all(class_="odd"):
+            torrent = {}
+            name = info.find(class_="title")
+            url = info.find(class_="quickdownload").find('a').get('href')
+            date = info.find(class_="name")
+            dateRegex = re.match("^.*(\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}[:]\d{2}).*$",str(date),re.DOTALL)
+            size = info.find_all('td')
+            sizeRegex = re.match("^.*[<td>](\d.*\s\w\w)[</td>].*$",str(size),re.DOTALL)
+            torrent['name'] = str(name.string)
+            torrent['url'] = 'http://torrentleech.org' + url
+            torrent['date'] = dateRegex.group(1)
+            torrent['size'] = sizeRegex.group(1)
+            torrentOdd.append(torrent)
 
     torrentList = torrentEven + torrentOdd
+
     print >> tmpFile, json.dumps(torrentList, sort_keys=True)
 
     tmpFile.close()
